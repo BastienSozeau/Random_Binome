7ba7bdf2dec84a8b434a3cc52799f005ba8cedbe
BastienSozeau <bastien.sozeau@gmail.com>
2020-01-25 20:45:26 +0100

    initial commit

---
+++ binome/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+import random
+
+# Create your views here.
+from django.http import HttpResponse
+from binome.models import Post
+from django import forms
+
+
+def binome_index(request):
+	posts = Post.objects.order_by('-id')
+	context = {
+		"posts": posts,
+	}
+	return render(request, "index.html", context)
+
+def submit(request):
+	result = {}
+	if request.method == 'POST':
+
+
+		form = YourForm = (request.POST)
+		liste = {'Imane' : form['select_Imane'],'Mohamed D' : form['select_Mohamed D'],'Yassine' : form['select_Yassine'],'Luis Felipe' : form['select_Luis Felipe'],'Cécile' : form['select_Cécile'],'Marc-Henri' : form['select_Marc-Henri'],'Gaëtan' : form['select_Gaëtan'],'Alexandre' : form['select_Alexandre'],'Tony' : form['select_Tony'],'Emmanuelle' : form['select_Emmanuelle'],'Nour' : form['select_Nour'],'Bastien' : form['select_Bastien'],'Mohamed S' : form['select_Mohamed S'],'Mathusha' : form['select_Mathusha'],'Laien' : form['select_Laien'],'Josef' : form['select_Josef'],'Nadia' : form['select_Nadia']}
+
+		binomes,resultbin,sep,dispo = [],'','—' * 30, [nom for nom in liste if liste[nom] == 'Disponible' ]
+		for nom in range(len(dispo)): 
+			while len(dispo) > 3:
+				nom = random.sample(dispo,2)
+				dispo.remove(nom[0])
+				dispo.remove(nom[1])
+				binomes.append(' & '.join([nom[0],nom[1]]))
+		binomes.append(' & '.join(dispo))
+		for i, binome in enumerate(binomes):
+			resultbin += '<h4>' + str(i+1) + ' ' + binome + '</h4>'
+
+		result = { 'binomes' : resultbin }
+	return render(request, "submit.html",result )
