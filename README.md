b245ed5e9b9dea1f681410d79d65f3f7d193189f
BastienSozeau <bastien.sozeau@gmail.com>
2020-01-26 12:06:44 +0100

    better heroku

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Random_Binome
-A simple website showcasting a Random Binome selector
+## Random_Binome
+
+<a align="center" href="http://binome.sozoo.fr"><img width="100%" src="https://raw.githubusercontent.com/BastienSozeau/Random_Binome/master/binome/static/img/screenshot.png" alt="BS_bootstrap"></a>
+
+
+My second Django app, a simple website showcasting a Random Binome selector  
+Random Binome is deployed on heroku here [binome.sozoo.fr](http://binome.sozoo.fr/) 
+
+
+## License
+
+Licensed under the [MIT License](https://github.com/BastienSozeau/BS_bootstrap/blob/master/LICENSE.md).
--- binome/views.py
+++ binome/views.py
@@ -22,14 +22,16 @@ def submit(request):
 		form = YourForm = (request.POST)
 		liste = {'Imane' : form['select_Imane'],'Mohamed D' : form['select_Mohamed D'],'Yassine' : form['select_Yassine'],'Luis Felipe' : form['select_Luis Felipe'],'Cécile' : form['select_Cécile'],'Marc-Henri' : form['select_Marc-Henri'],'Gaëtan' : form['select_Gaëtan'],'Alexandre' : form['select_Alexandre'],'Tony' : form['select_Tony'],'Emmanuelle' : form['select_Emmanuelle'],'Nour' : form['select_Nour'],'Bastien' : form['select_Bastien'],'Mohamed S' : form['select_Mohamed S'],'Mathusha' : form['select_Mathusha'],'Laien' : form['select_Laien'],'Josef' : form['select_Josef'],'Nadia' : form['select_Nadia']}
 
-		binomes,resultbin,sep,dispo = [],'','—' * 30, [nom for nom in liste if liste[nom] == 'Disponible' ]
+		binomes,resultbin,sep,dispo,restdispo = [],'','—' * 30, [nom for nom in liste if liste[nom] == 'Disponible' ],[]
 		for nom in range(len(dispo)): 
 			while len(dispo) > 3:
 				nom = random.sample(dispo,2)
 				dispo.remove(nom[0])
 				dispo.remove(nom[1])
-				binomes.append(' & '.join([nom[0],nom[1]]))
-		binomes.append(' & '.join(dispo))
+				binomes.append(' & '.join(['<span class="rd"><i class="icon-user"></i>'+nom[0]+'</span>','<span class="rd"><i class="icon-user"></i>'+nom[1]+'</span>']))
+		for nom in dispo: 
+			restdispo.append('<span class="rd"><i class="icon-user"></i>'+nom+'</span>')
+		binomes.append(' & '.join(restdispo))
 		for i, binome in enumerate(binomes):
 			resultbin += '<h4>' + str(i+1) + ' ' + binome + '</h4>'
 
